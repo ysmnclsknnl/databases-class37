4f305d3caf8cf74aeb568afcbb8ed925c474c1e4
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-06-28 19:17:56 +0200

    sql queries  written in uppercase

--- Week1/homework/meetup.js
+++ Week1/homework/meetup.js
@@ -17,12 +17,12 @@ const execute_queries = (arrayOfQueries) => {
 
 connection.connect();
 const queries = [
-  "create table Invitee (invitee_no int primary key auto_increment, invitee_name VARCHAR(100) unique, invited_by VARCHAR(100))",
-  "create table Room (room_no int primary key auto_increment not null, room_name VARCHAR(100) unique not null, floor_number int not null)",
-  "create table Meeting (meeting_no int primary key auto_increment not null, meeting_title VARCHAR(100) unique not null, starting_time DATETIME not null,ending_time DATETIME not null, room_no int, foreign key(room_no) references Room(room_no))",
-  "insert into Invitee(invitee_name,invited_by)values('Yasemin Caliskan','Gunay Caliskan'),('Harun Caliskan','Cristiano Ronaldo'),('John Doe','Fede Fusco'),('Natalie Ducas','Melih Kibar'),('Roberto Carlos','Lionel Messi')",
-  "insert into Room(room_name,floor_number) values ('Orange',0),('Blue',0),('Green',1),('Black',1),('Moonlit',2),('Space',2)",
-  "insert into Meeting(meeting_title,starting_time,ending_time,room_no) values ('Meet Up','2022-06-28 14:00:00','2022-06-28 15:00:00',1),('Briefing','2022-06-29 14:00:00','2022-06-29 15:00:00',2),('Evaluation','2022-06-29 16:00:00','2022-06-29 17:00:00',3),('Project','2022-06-29 16:00:00','2022-06-29 17:00:00',4),('Orientation','2022-06-30 16:00:00','2022-06-30 17:00:00',5)",
+  "CREATE TABLE Invitee (invitee_no INT PRIMARY KEY AUTO_INCREMENT, invitee_name VARCHAR(100) UNIQUE, invited_by VARCHAR(100))",
+  "CREATE TABLE Room (room_no INT PRIMARY KEY AUTO_INCREMENT NOT NULL, room_name VARCHAR(100) UNIQUE NOT NULL, floor_number INT NOT NULL)",
+  "CREATE TABLE Meeting (meeting_no INT PRIMARY KEY AUTO_INCREMENT NOT NULL, meeting_title VARCHAR(100) UNIQUE NOT NULL, starting_time DATETIME NOT NULL,ending_time DATETIME NOT NULL, room_no INT, FOREIGN KEY(room_no) REFERENCES Room(room_no))",
+  "INSERT INTO Invitee(invitee_name,invited_by)VALUES('Yasemin Caliskan','Gunay Caliskan'),('Harun Caliskan','Cristiano Ronaldo'),('John Doe','Fede Fusco'),('Natalie Ducas','Melih Kibar'),('Roberto Carlos','Lionel Messi')",
+  "INSERT INTO Room(room_name,floor_number) VALUES ('Orange',0),('Blue',0),('Green',1),('Black',1),('Moonlit',2),('Space',2)",
+  "INSERT INTO Meeting(meeting_title,starting_time,ending_time,room_no) VALUES ('Meet Up','2022-06-28 14:00:00','2022-06-28 15:00:00',1),('Briefing','2022-06-29 14:00:00','2022-06-29 15:00:00',2),('Evaluation','2022-06-29 16:00:00','2022-06-29 17:00:00',3),('Project','2022-06-29 16:00:00','2022-06-29 17:00:00',4),('Orientation','2022-06-30 16:00:00','2022-06-30 17:00:00',5)",
 ];
 
 execute_queries(queries);
