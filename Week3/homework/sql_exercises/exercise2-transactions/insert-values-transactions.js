b171a678139f5ce2e8966f55b3b31b87ba623760
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-07-12 15:41:15 +0200

    now method is used

--- Week3/homework/sql_exercises/exercise2-transactions/insert-values-transactions.js
+++ Week3/homework/sql_exercises/exercise2-transactions/insert-values-transactions.js
@@ -8,7 +8,7 @@ const transfer_transaction = [
   "START TRANSACTION",
   "UPDATE account SET balance=balance-1000 WHERE account_number=101",
   "UPDATE account SET balance=balance+1000 WHERE account_number=102",
-  "INSERT INTO account_changes(account_number, amount, changed_date, remark) VALUES(101,1000,'2021-07-11','education fee'),(102,1000,'2021-07-11','education fee')",
+  "INSERT INTO account_changes(account_number, amount, changed_date, remark) VALUES(101,1000,now(),'education fee'),(102,1000,now(),'education fee')",
 ];
 
 export { insert_account, transfer_transaction };
