4c7cc40893c5a4927935b4e2e292acbe9b7cc00d
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-07-18 21:10:53 +0200

    data.map function is removed.

--- Week4/homework/ex2-transactions/setup.js
+++ Week4/homework/ex2-transactions/setup.js
@@ -22,31 +22,8 @@ const seedDatabase = async (client) => {
   // Remove all the documents
   await accountsCollection.deleteMany({});
 
-  console.log(`data is `);
-  console.log(data);
-  // Convert data to array version of elements
-  const documents = data.map((dataItem) => {
-    const { account_number, balance, account_changes } = dataItem;
-    console.log(`data item is ${dataItem}`);
-
-    //   const depictionElementKeys = Object.keys(dataItem).filter(
-    //     (key) => !["account_number", "balance", "account_changes"].includes(key)
-    //   );
-
-    //   const depictionElements = depictionElementKeys.filter(
-    //     (key) => dataItem[key] === 1
-    //   );
-
-    return {
-      account_number: account_number,
-      // Remove the extra quotation marks
-      balance: balance,
-      account_changes: account_changes,
-    };
-  });
-
   // Add our documents
-  await accountsCollection.insertMany(documents);
+  await accountsCollection.insertMany(data);
 };
 
 module.exports = { seedDatabase };
