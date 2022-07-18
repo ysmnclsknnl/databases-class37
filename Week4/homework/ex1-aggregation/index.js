4d81bea9a8eb07b848ac433d4c2c808e7d41624c
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-07-18 12:17:01 +0200

    exercise 1 is done successfully

---
+++ Week4/homework/ex1-aggregation/index.js
@@ -0,0 +1,87 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function getTotalPopulationOfTheCountryPerYear(client, country) {
+  const pipeline = [
+    {
+      $match: {
+        Country: country,
+      },
+    },
+    {
+      $group: {
+        _id: "$Year",
+        countPopulation: {
+          $sum: {
+            $sum: ["$F", "$M"],
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        _id: 1,
+      },
+    },
+  ];
+  const aggCursor = client
+    .db("databaseWeek4")
+    .collection("population_pyramid")
+    .aggregate(pipeline);
+
+  await aggCursor.forEach((result) => console.log(result));
+}
+
+async function getTotalPopulationForAnAgeRangeandAYear(client, ageRange, year) {
+  const pipeline = [
+    {
+      $match: {
+        Year: year,
+        Age: ageRange,
+      },
+    },
+    {
+      $addFields: {
+        TotalPopulation: {
+          $sum: ["$F", "$M"],
+        },
+      },
+    },
+  ];
+  const aggCursor = client
+    .db("databaseWeek4")
+    .collection("population_pyramid")
+    .aggregate(pipeline);
+
+  await aggCursor.forEach((result) => {
+    console.log(result);
+  });
+}
+
+async function main() {
+  if (process.env.MONGODB_URL == null) {
+    throw Error(
+      `You did not set up the environment variables correctly. Did you create a '.env' file and add a package to create it?`
+    );
+  }
+  const client = new MongoClient(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+
+  try {
+    await client.connect();
+
+    await getTotalPopulationOfTheCountryPerYear(client, "Netherlands");
+
+    await getTotalPopulationForAnAgeRangeandAYear(client, "100+", 2020);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.close();
+  }
+}
+
+main();
