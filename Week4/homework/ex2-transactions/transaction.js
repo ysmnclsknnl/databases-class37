9cc476f8784b11271465e75c507150e6a402de11
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-07-19 19:43:55 +0200

    helper function is imported

--- Week4/homework/ex2-transactions/transaction.js
+++ Week4/homework/ex2-transactions/transaction.js
@@ -1,27 +1,50 @@
+const { getAccountChanges } = require("./helpers.js");
+
 async function transferCredits(
   client,
+  change_id,
   fromAccountId,
   toAccountId,
   amount,
   date,
   remark
 ) {
+  const { remitterAccountChanges, remitteeAccountChanges } = getAccountChanges(
+    change_id,
+    amount,
+    date,
+    remark
+  );
+
   const accountsCollection = client.db("databaseWeek4").collection("accounts");
   const session = client.startSession();
 
   try {
     await session.withTransaction(async () => {
       // Remove from fromUser
+
       await accountsCollection.updateOne(
         { account_number: fromAccountId },
-        { $inc: { balance: amount * -1 } },
+        {
+          $inc: { balance: amount * -1 },
+          $addToSet: {
+            account_changes: remitterAccountChanges,
+          },
+        },
+
         { session }
       );
 
       // Add to toUser
       await accountsCollection.updateOne(
         { account_number: toAccountId },
-        { $inc: { balance: amount } },
+        {
+          $inc: { balance: amount },
+          $addToSet: {
+            account_changes: remitterAccountChanges,
+          },
+        },
+
         { session }
       );
     });
